f549d401179be89eff5ed8e0c852706d042f447c
Nurullah Demir <nurullah.demir1453@gmail.com>
2022-11-25 23:21:58 +0300

    ders sonu

---
+++ src/ders45_Maps/C01_Maps.java
@@ -0,0 +1,46 @@
+package ders45_Maps;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class C01_Maps {
+
+    public static void main(String[] args) {
+
+        //Verilen bir cümlede kullanılan her bir harfi ve o harfin kullanım miktarını
+        //A=5 B=3 gibi yazdırın.
+
+        String str="Javayı balonu patlatmadan bitirelim";
+        //J=1 a=7 v=1 y=1......
+
+        String [] harflerArr=str.split("");
+        //[J, a, v, a, y, ı,  , b, a, l, o, n, u,  , p, a, t, l, a, t, m, a, d, a, n,  , b, i, t, i, r, e, l, i, m]
+
+        Map<String,Integer>kullanımMiktarlarıMap=new TreeMap<>();
+        int sayac=1;
+
+        //1-tüm harfleri sırasıyla ele alacağız.
+
+        for (int i = 0; i <harflerArr.length ; i++) {
+
+            //2- ele aldıgımız harfin daha önce mape eklenip eklenmediğini kontrol edelim.
+            if (kullanımMiktarlarıMap.containsKey(harflerArr[i])){
+                continue;
+            }else {
+                //Kod buraya geldiyse o harf henüz mape eklenmemiştir.
+                //o zaman bu harfin kullanım miktarını bulmalıyız.
+                for (int j = i+1; j <harflerArr.length ; j++) {
+                    if (harflerArr[j].equals(harflerArr[i])){
+                        sayac++;
+                    }
+                }
+                //sayma işlemi bitti Key ve valueyi mape ekleyelim
+                kullanımMiktarlarıMap.put(harflerArr[i],sayac);
+                sayac=1;
+            }
+        }
+        System.out.println(kullanımMiktarlarıMap);
+    }
+}
---
+++ src/ders45_Maps/C02_Maps.java
@@ -0,0 +1,22 @@
+package ders45_Maps;
+
+import ders44_maps.MapDepo;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class C02_Maps {
+    public static void main(String[] args) {
+
+        //öğrenciler mapinde şubesi H olan öğrencilerin isim soyisim ve sınıflarını yazdıralım.
+
+        Map<Integer,String>ögrenciMap=MapDepo.ornekMapOlustur();
+
+        Set<Integer> ögrenciKeyleri= ögrenciMap.keySet(); //Bir mapdeki keyleri set olarak döndürür.
+
+        Collection<String>ögrenciValueCollection =ögrenciMap.values(); //Bir mapdeki tüm valueleri bize collection olarak döndürür.
+
+
+    }
+}
